73a6798047a5f5122cfcf135dae8ecdeef25f732
Roman Gurovich <romangurovich@gmail.com>
2016-11-12 20:32:01 -0800

    weather endpoint

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,5 +1,18 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+import requests
+import json
 
 class LocationViewSet(viewsets.ModelViewSet):
-	
+
+def weather(request):
+    root_uri = 'https://api.darksky.net'
+    api = '0123456789abcdef9876543210fedcba'
+    action = 'forecast'
+    lat = request.GET['lat']
+    lng = request.GET['lng']
+    lat_lng = ','.join((lat, lng))
+    uri = '/'.join((root_uri, action, api, lat_lng))
+    response = requests.get(uri)
+    return Response(response.json())
